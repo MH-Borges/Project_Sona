347af9a97330f247df82343ab956288277dcc7cc
matheus <matheinhosilva.borges@hotmail.com>
2020-11-20 22:33:06 -0300

    banner inicial

---
+++ jquery.js
@@ -0,0 +1,40 @@
+/*Menu effeito*/
+$(window).scroll(function(event){
+
+    const efeito = window.pageYOffset;
+    const posicao = 100;
+    if (efeito > posicao){
+        $(".menu").addClass('active');
+        $(".logo").addClass('active');
+        $("nav ul").addClass('active');
+        $(".MenuBT").addClass('active');
+    }else{
+        $(".menu").removeClass('active');
+        $(".logo").removeClass('active');
+        $("nav ul").removeClass('active');
+        $(".MenuBT").removeClass('active');
+    }
+});
+/*Menu effeito*/
+
+/*botão effeito*/
+jQuery(document).ready(function(){
+
+    jQuery("#btn_UP").hide();
+    
+    jQuery('a#btn_UP').click(function () {
+             jQuery('body,html').animate({
+               scrollTop: 0
+             }, 800);
+            return false;
+       });
+    
+    jQuery(window).scroll(function () {
+             if (jQuery(this).scrollTop() > 1000) {
+                jQuery('#btn_UP').fadeIn();
+             } else {
+                jQuery('#btn_UP').fadeOut();
+             }
+         });
+    });
+    
---
+++ script.js
@@ -0,0 +1,56 @@
+/*transparencia menu*/
+const scrollM = document.getElementById('#banner');
+
+function corMenu(){
+    document.querySelectorAll('.menu').style.backgroundColor = "rgb(0, 0, 0)"; 
+}
+/*transparencia menu*/
+
+
+/*rolamento menu*/ 
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const to = getScrollTopByHref(event.currentTarget)- 100;
+    scrollToPosition(to);
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to){
+    smoothScrollTo(0, to);
+}
+
+function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+
+    duration = typeof duration !== 'undefined' ? duration : 600;
+
+const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+};
+
+const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
+/*rolamento menu*/
